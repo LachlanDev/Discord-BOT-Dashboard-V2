d09243699baa8b8f4fd74caa414f5469b71004fe
PapaSnags <58458169+LachlanDev@users.noreply.github.com>
2021-04-05 23:12:59 +1000

    🔒 Authentication check

---
+++ src/config/auth.js
@@ -0,0 +1,14 @@
+module.exports = {
+    ensureAuthenticated: function(req, res, next) {
+      if (req.isAuthenticated()) {
+        return next();
+      }
+      res.redirect('/login');
+    },
+    forwardAuthenticated: function(req, res, next) {
+      if (!req.isAuthenticated()) {
+        return next();
+      }
+      res.redirect('/home');      
+    }
+};
