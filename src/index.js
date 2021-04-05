06b13ee7fcd135b5d702f92aa2ed77743aa44156
PapaSnags <58458169+LachlanDev@users.noreply.github.com>
2021-04-05 23:14:09 +1000

    🖥️ Webserver

---
+++ src/index.js
@@ -0,0 +1,61 @@
+const express = require('express')
+const discord = require('./bot')
+const flash = require('connect-flash');
+const session = require('express-session');
+const passport = require('passport');
+
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+port = 3000;
+
+app.use(express.static('./public'));
+app.set('view engine', 'ejs');
+
+require('./config/passport')(passport);
+
+
+// Express session
+app.use(
+    session({
+      secret: '4135231b7f33c66406cdb2a78420fa76',
+      resave: true,
+      saveUninitialized: true
+    })
+);
+  
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Connect flash
+app.use(flash());
+
+// Global variables
+app.use(function(req, res, next) {
+  res.locals.success = req.flash('success');
+  res.locals.error = req.flash('error');
+  next();
+});
+
+app.use('/', require('./routes/index.js'));
+app.use('/login', require('./routes/login.js'));
+
+http.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
+io.sockets.on('connection', function(sockets){
+  setInterval(function(){ 
+    // Uptime Count
+    let days = Math.floor(discord.client.uptime / 86400000);
+    let hours = Math.floor(discord.client.uptime / 3600000) % 24;
+    let minutes = Math.floor(discord.client.uptime / 60000) % 60;
+    let seconds = Math.floor(discord.client.uptime / 1000) % 60;
+  
+    var BOTuptime = `${days}d ${hours}h ${minutes}m ${seconds}s` 
+    
+    // Emit count to browser 
+    sockets.emit('uptime',{uptime:BOTuptime}); }, 1000);
+})
