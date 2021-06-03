73833839ba074b6845a510453917459bc7ebf135
PapaSnags <58458169+LachlanDev@users.noreply.github.com>
2021-06-03 22:47:07 +1000

    Update ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,5 +1,24 @@
 const chalk = require("chalk")
 const config = require("../config/config.json")
+const vers = require("../config/version.json")
+const discord = require('../bot')
+var figlet = require('figlet');
+const lolcatjs = require('lolcatjs');
+
 module.exports = (client) => {
-    console.log(chalk.magenta(`Bot Made by LachlanDev#8014 \nPrefix is ${config.prefix}`));
+    console.clear()
+    var banner = figlet.textSync('Discord BOT Dashboard V2', {
+        font: 'small',
+        horizontalLayout: 'default',
+        width: 1000,
+        whitespaceBreak: true
+    });
+    lolcatjs.fromString(banner);
+    console.log(chalk.bold.green('Launched Succesfully...'))
+    console.log(chalk.magenta('Version:'),chalk.cyan(`${vers.ver}`))
+    console.log(chalk.magenta('Made by:'),chalk.cyan('LachlanDev#8014'))
+    console.log(chalk.magenta('Prefix:'),chalk.cyan(`${config.prefix}\n`))
+    console.log(chalk.green(chalk.bold(`${discord.client.user.username}`),`is online!`))
+    console.log(chalk.green(chalk.bold(`Dashboard:`),`http://localhost:3000`))
+
 }
