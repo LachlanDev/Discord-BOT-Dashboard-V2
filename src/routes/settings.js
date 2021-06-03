fbb64651180285326ebd954ea0a7f02ac6410588
PapaSnags <58458169+LachlanDev@users.noreply.github.com>
2021-06-03 22:46:34 +1000

    Create settings.js

---
+++ src/routes/settings.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router = express.Router();
+const discord = require('../bot')
+const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
+const config = require('../config/config.json')
+const version = require('../config/version.json')
+
+json = require('json-update');
+const jsonfile = require('jsonfile')
+const file = "./config/config.json"
+
+
+router.get('/settings', ensureAuthenticated,(req, res) => {
+    var config = jsonfile.readFileSync(file);
+    res.render('home/settings',{
+        profile:req.user,
+        client:discord.client,
+        config:config,
+        version:version
+    })
+})
+
+router.post('/settings/config',ensureAuthenticated,(req,res) =>{
+    // Need to add ADMIN
+    json.update('./config/config.json',{clientID:`${req.body.clientID}`,clientSecret:`${req.body.clientSecret}`,callbackURL:`${req.body.callbackURL}`,Admin:req.body.admin.split(','),token:`${req.body.token}`,prefix:`${req.body.prefix}`}).then(function(dat) { 
+        req.flash('success', 'Config Updated please now restart the application!')
+        res.redirect('/settings')
+    })
+})
+  
+module.exports = router;
