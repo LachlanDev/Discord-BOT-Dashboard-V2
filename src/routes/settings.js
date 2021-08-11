15b1d731ba4010dbd455353a0ca793c8b5255cb5
PapaSnags <58458169+LachlanDev@users.noreply.github.com>
2021-08-11 13:38:34 +1000

    Added port to update config function

--- src/routes/settings.js
+++ src/routes/settings.js
@@ -21,7 +21,7 @@ router.get('/settings', ensureAuthenticated,(req, res) => {
 })
 
 router.post('/settings/config',ensureAuthenticated,(req,res) =>{
-    json.update('./config/config.json',{clientID:`${req.body.clientID}`,clientSecret:`${req.body.clientSecret}`,callbackURL:`${req.body.callbackURL}`,Admin:req.body.admin.split(','),token:`${req.body.token}`,prefix:`${req.body.prefix}`}).then(function(dat) { 
+    json.update('./config/config.json',{clientID:`${req.body.clientID}`,clientSecret:`${req.body.clientSecret}`,callbackURL:`${req.body.callbackURL}`,Admin:req.body.admin.split(','),token:`${req.body.token}`,prefix:`${req.body.prefix}`,port:`${req.body.port}`}).then(function(dat) { 
         req.flash('success', 'Config Updated please now restart the application!')
         res.redirect('/settings')
     })
