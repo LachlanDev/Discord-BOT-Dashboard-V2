d95e09b485e324710ddd84aa50d4488b5c04d16d
PapaSnags <58458169+LachlanDev@users.noreply.github.com>
2021-08-11 13:31:38 +1000

    Added custom port configuration

--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ const flash = require('connect-flash');
 const session = require('express-session');
 const passport = require('passport');
 const fileUpload = require('express-fileupload');
+const config = require('./config/config.json')
 
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
-port = 3000;
+port = config.port;
 
 app.use(express.static('./public'));
 app.set('view engine', 'ejs');
