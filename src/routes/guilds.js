c0158fb07c2c1cd898f3ef952de5ccc2b3f52989
LachlanDev <58458169+LachlanDev@users.noreply.github.com>
2021-08-12 12:46:29 +1000

    Added theme file

--- src/routes/guilds.js
+++ src/routes/guilds.js
@@ -4,15 +4,19 @@ const { ensureAuthenticated, forwardAuthenticated } = require('../auth/auth');
 const discord = require('../bot')
 const dateformat = require('dateformat')
 const number = require('easy-number-formatter')
+const themes = "./config/theme.json"
+const jsonfile = require('jsonfile')
 
 router.get('/guilds',ensureAuthenticated,(req,res) =>{
+    var theme = jsonfile.readFileSync(themes);
     let guilds = discord.client.guilds.cache.array()
     res.render('home/guilds',{
         guilds:guilds,
         profile:req.user,
         client:discord.client,
         dateformat:dateformat,
-        number:number
+        number:number,
+        theme:theme
     })
 })
 
--- src/routes/support.js
+++ src/routes/support.js
@@ -2,11 +2,15 @@ const express = require('express');
 const router = express.Router();
 const discord = require('../bot')
 const { ensureAuthenticated, forwardAuthenticated } = require('../auth/auth');
+const themes = "./config/theme.json"
+const jsonfile = require('jsonfile')
 
 router.get('/support', ensureAuthenticated,(req, res) => {
+    var theme = jsonfile.readFileSync(themes);
     res.render('home/support',{
         profile:req.user,
-        client:discord.client
+        client:discord.client,
+        theme:theme
     })
 })
 
