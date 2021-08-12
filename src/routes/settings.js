19cf080258264827cd0606bea2bdfeb7f108ebad
LachlanDev <58458169+LachlanDev@users.noreply.github.com>
2021-08-12 14:11:42 +1000

    Added theme upload function

--- src/routes/settings.js
+++ src/routes/settings.js
@@ -40,5 +40,35 @@ router.post('/settings/dashboard',ensureAuthenticated,(req,res) =>{
         res.redirect('/settings')
     })
 })
+
+router.post('/settings/upload/theme', ensureAuthenticated,function(req, res) {
+    let sampleFile;
+    let uploadPath;
+  
+    if (!req.files || Object.keys(req.files).length === 0) {
+      return req.flash('error', `No file was uploaded, please try again!`), 
+      res.redirect('/settings')
+    }
+    if(!req.files.sampleFile.name.endsWith(".css")){
+      return req.flash('error', `Please only upload CSS files!`), 
+      res.redirect('/settings')
+    }
+    const path = './themes/' + req.files.sampleFile.name
+    if(fs.existsSync(path)) {
+      return req.flash('error', `Theme with that name already exists!`), 
+      res.redirect('/settings')
+    }
+
+    sampleFile = req.files.sampleFile;
+    uploadPath = './themes/' + sampleFile.name;
+  
+    sampleFile.mv(uploadPath, function(err) {
+      if (err)
+        return res.status(500).send(err);
+  
+        req.flash('success', `Theme ${sampleFile.name} successfully uploaded!`)
+        res.redirect('/settings')
+    });
+});
   
 module.exports = router;
